9d47042c4367de5441a90481afe2a06216576456
VIJAYA LAKSHMI Devoju <vijaya.lakshmi.devoju@ibm.com>
2021-04-22 15:53:12 +0530

    Created a.cpp

---
+++ b.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int val;
+    cout<<"Enter the Number: ";
+    cin>>val;
+    cout<<"\nThe Value is "<<val;
+    cout<<endl;
+    return 0;
+}
---
+++ c.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num=10;
+    cout<<"The Value of 'num' is "<<num;
+    cout<<endl;
+    return 0;
+}
